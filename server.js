2cdf134591d98a36b750eb4770e4e8f5df70755e
Dung <trungdungr9000@gmail.com>
2020-08-05 16:34:58 +0700

    useragent

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ cors_proxy.createServer({
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
+    "User-Agent":"xxxxx"
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
